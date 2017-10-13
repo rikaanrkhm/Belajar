2bdad12b08c3c14cf7e8eb4a83aa2976bdc7b976
rikaanrokhim <rikaanrokhim1@gmail.com>
2017-10-13 09:07:29 +0700

    Belajar

---
+++ new.php
@@ -0,0 +1,12 @@
+<?php
+
+class mawar
+	{
+		public $warna = 'merah';
+		public $daun = 'hijau';
+	}
+	
+$mawar = new mawar;
+echo 'Mawar warnaynya..' . $mawar->warna . '<br>' . 'Daunnya..' . $mawar->daun;
+
+?>
